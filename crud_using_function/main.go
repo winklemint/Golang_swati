ba980f7c8046e2ef200d7f1de8e005d44ea42d54
winklemint <swatichouhan39@gmail.com>
2023-05-01 00:43:42 +0530

    commit -redo

--- crud_using_function/main.go
+++ crud_using_function/main.go
@@ -8,7 +8,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type Employee struct {
+type employee struct {
 	ID      int
 	Ename   string
 	Esalary int
@@ -32,7 +32,7 @@ func CreateDatabase(w http.ResponseWriter, r *http.Request) {
 
 func CreateTable(w http.ResponseWriter, r *http.Request) {
 	db := dbConnect()
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS employee(ID PRIMARY KEY NOT NULL AUTO_INCREMENT, Ename VARCHAR(50) , Esalary INT)")
+	_, err := db.Exec("CREATE TABLE employee(ID PRIMARY KEY NOT NULL AUTO_INCREMENT, Ename VARCHAR(50) , Esalary INT)")
 	if err != nil {
 		panic(err)
 	}
@@ -40,18 +40,18 @@ func CreateTable(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Table created ")
 }
 
-// func InsertTable(w http.ResponseWriter, r *http.Request){
-// 	db := dbConnect()
-// 	_,err := db.Exec("INSERT INTO employee(Ename , Esalary) VALUES('Swapnil' , 45)")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Println("Values inserted into table")
-// 	fmt.Fprintf(w, "Data Inserted")
-//}
+func InsertTable(w http.ResponseWriter, r *http.Request) {
+	db := dbConnect()
+	_, err := db.Exec("INSERT INTO employee(Ename , Esalary) VALUES('Swapnil' , 45)")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Values inserted into table")
+	fmt.Fprintf(w, "Data Inserted")
+}
 
 func main() {
 	http.HandleFunc("/createdb", CreateDatabase)
 	http.HandleFunc("/createtable", CreateTable)
-	fmt.Println(http.ListenAndServe(":8080", nil))
+	fmt.Println(http.ListenAndServe(":8763", nil))
 }
