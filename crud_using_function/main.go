1c1a23111df41c045d12cef3e85c13c53c4c660b
winklemint <swatichouhan39@gmail.com>
2023-05-03 17:52:19 +0530

    Update main.go

--- crud_using_function/main.go
+++ crud_using_function/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -27,46 +28,74 @@ type employee struct {
 // }
 
 func dbConn() (db *sql.DB) {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/forestdb")
 	if err != nil {
 		panic(err)
 	}
 	return db
 }
 
-func CreateDatabase(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	_, err := db.Exec("CREATE DATABASE WalkingDreamzdb")
-	if err != nil {
-		panic(err)
-	}
-}
+// func CreateDatabase(w http.ResponseWriter, r *http.Request) {
+// 	db := dbConn()
+// 	_, err := db.Exec("CREATE DATABASE WalkingDreamzdb")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
 
-func CreateTable(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	_, err := db.Exec("CREATE TABLE Employee(ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, Ename VARCHAR(50), Esalary INT))")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("table created")
-	fmt.Fprintln(w, "Table created ")
-}
+// func CreateTable(w http.ResponseWriter, r *http.Request) {
+// 	db := dbConn()
+// 	_, err := db.Exec("CREATE TABLE employee(ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, Ename VARCHAR(50), Esalary INT))")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("table created")
+// 	fmt.Fprintln(w, "Table created ")
+// }
 
 func InsertTable(w http.ResponseWriter, r *http.Request) {
+
 	db := dbConn()
-	_, err := db.Exec("INSERT INTO employee(Ename , Esalary) VALUES('Swapnil' , 45)")
+	_, err := db.Exec("INSERT INTO employee(Ename , Esalary) VALUES('hitesh' , 47)")
 	if err != nil {
 		panic(err)
 	}
+	return
 	fmt.Println("Values inserted into table")
-	fmt.Fprintf(w, "Data Inserted")
+	fmt.Fprintln(w, "Data Inserted")
+
+}
+func getEmployee(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	rows, err := db.Query("SELECT * FROM employee")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var e employee
+		err := rows.Scan(&e.ID, &e.Ename, &e.Esalary)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Fprintf(w, "ID: %d, Name: %s, Salary: %d",
+			e.ID, e.Ename, e.Esalary)
+
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+	}
 }
 
 func main() {
-	dbConn()
 
-	//log.Println("Server started on: http://localhost:8080")
-	http.HandleFunc("/", CreateTable)
-	http.HandleFunc("/inserttable", InsertTable)
-	http.ListenAndServe(":8080", nil)
+	log.Println("Server started on: http://localhost:8080")
+
+	//http.HandleFunc("/createtable", CreateTable)
+
+	http.HandleFunc("/", getEmployee)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
